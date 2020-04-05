9bc4d36d54a09b1f0867ee856c06b2bb4422d052
Lennie Araki <laraki@thermal.com>
2020-04-05 15:15:19 -0700

    Change Shanghai to layout 8 players Add Discard Pile logic for "old buys"

--- shanghai.js
+++ shanghai.js
@@ -5,7 +5,7 @@ cards.init({table:'#card-table', cardSize: {width:46,height:62, padding:12}, car
 //Create a new deck of cards
 deck = new cards.Deck(); 
 //By default it's in the middle of the container, put it slightly to the side
-deck.x -= 50;
+deck.x -= 100;
 
 // cards.all contains all cards, put them all in the deck
 deck.addCards(cards.all); 
@@ -14,23 +14,41 @@ deck.addCards(cards.all);
 deck.render({immediate:true});
 
 //Now lets create a couple of hands, one face down, one face up.
-hand1 = new cards.Hand({faceUp:true, y:60});
-hand2 = new cards.Hand({faceUp:true, x:700, y:300});
-hand3 = new cards.Hand({faceUp:true, y:540});
-hand4 = new cards.Hand({faceUp:true, x:100, y:300});
-hand2.angle = 90;
-hand4.angle = 270;
+var x1 = 180;
+var x2 = 550;
+var x3 = 920;
+var y1 = 50;
+var y2 = 550;
+
+hand1 = new cards.Hand({faceUp:true, x:x1, y:y1});
+hand2 = new cards.Hand({faceUp:true, x:x2, y:y1});
+hand3 = new cards.Hand({faceUp:true, x:x3, y:y1});
+hand4 = new cards.Hand({faceUp:true, x:1060, y:300});
+hand5 = new cards.Hand({faceUp:true, x:x3, y:y2});
+hand6 = new cards.Hand({faceUp:true, x:x2, y:y2});
+hand7 = new cards.Hand({faceUp:true, x:x1, y:y2});
+hand8 = new cards.Hand({faceUp:true, x:50, y:300});
+
+hand4.angle = 90;
+hand8.angle = 270;
 
 //Lets add a discard pile
 discardPile = new cards.Deck({faceUp:true});
-discardPile.x += 50;
+discardPile.x -= 200;
+var xdiscard = 650;
+var ydiscard = 200;
 
+// Create discard piles (per suit)
+discardSpades = new cards.Hand({faceUp:true, x:xdiscard, y:ydiscard});
+discardHearts = new cards.Hand({faceUp:true, x:xdiscard, y:ydiscard + 75});
+discardDiamonds = new cards.Hand({faceUp:true, x:xdiscard, y:ydiscard+150});
+discardClubs = new cards.Hand({faceUp:true, x:xdiscard, y:ydiscard+225});
 
 //Let's deal when the Deal button is pressed:
 $('#deal').click(function() {
 	//Deck has a built in method to deal to hands.
 	$('#deal').hide();
-	deck.deal(19, [hand1, hand2, hand3, hand4], 50, function() {
+	deck.deal(19, [hand1, hand2, hand3, hand4, hand5, hand6, hand7, hand8], 50, function() {
 		//This is a callback function, called when the dealing
 		//is done.
 		discardPile.addCard(deck.topCard());
@@ -43,21 +61,43 @@ $('#deal').click(function() {
 //to your hand
 deck.click(function(card){
 	if (card === deck.topCard()) {
-		hand3.addCard(deck.topCard());
-		hand3.render();
+		hand6.addCard(deck.topCard());
+		hand6.render();
+	}
+});
+
+// When you click on the discard pile card is added to your hand
+discardPile.click(function(card) {
+	if (card == discardPile.topCard()) {
+		hand6.addCard(discardPile.topCard());
+		hand6.render();
 	}
 });
 
 //Finally, when you click a card in your hand, if it's
 //the same suit or rank as the top card of the discard pile
 //then it's added to it
-hand3.click(function(card){
-	if (card.suit == discardPile.topCard().suit 
-		|| card.rank == discardPile.topCard().rank) {
-		discardPile.addCard(card);
-		discardPile.render();
-		hand3.render();
+hand6.click(function(card){
+	if (discardPile.length > 0) {
+		var discard = discardPile.topCard();
+		var suit = discard.suit;
+		if (suit == 's') {
+			discardSpades.addCard(discard);
+			discardSpades.render();
+		} else if (suit == 'h') {
+			discardHearts.addCard(discard)
+			discardHearts.render();
+		} else if (suit == 'd') {
+			discardDiamonds.addCard(discard);
+			discardDiamonds.render();
+		} else {
+			discardClubs.addCard(discard);
+			discardClubs.render();
+		}
 	}
+	discardPile.addCard(card);
+	discardPile.render();
+	hand6.render();
 });
 
 
